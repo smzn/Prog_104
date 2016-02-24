5c9d68f9060873dd39b3f8f1b14f69b2d095682a
smzn <s.mzn.eng@gmail.com>
2016-02-24 16:01:10 +0900

    Upload 2016/02/24

---
+++ app/src/main/java/com/example/mizuno/prog_104/Prog104Activity.java
@@ -0,0 +1,60 @@
+package com.example.mizuno.prog_104;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
+public class Prog104Activity extends AppCompatActivity {
+
+    public TextView text;
+    public EditText editText;
+    public String filepath;
+    public ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_prog104);
+
+        text = (TextView) findViewById(R.id.textView);
+        editText=(EditText) findViewById(R.id.editText);
+        Button button=(Button) findViewById(R.id.button);
+        imageView=(ImageView) findViewById(R.id.imageView);
+
+        //初期ディレクトリの指定と表示
+        filepath= Environment.getExternalStorageDirectory().toString() +"/"+ Environment.DIRECTORY_PICTURES+"/" ;
+        text.setText(filepath);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String filename = editText.getText().toString();
+                text.setText(filepath + filename);
+                File file = new File(filepath + filename);
+
+                try{
+                    InputStream is = new FileInputStream(file);
+                    Bitmap bm = BitmapFactory.decodeStream(is);
+                    imageView.setImageBitmap(bm);
+                }catch (FileNotFoundException e){
+                    text.setText("画像がありません");
+                    e.printStackTrace();
+                }
+                editText.setText(null);
+            }
+        });
+
+    }
+}
